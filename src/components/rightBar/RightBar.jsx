99c6ac77b1e9acb27f45334c3d8f738d5284a151
Markz2137 <marcuspoole200@gmail.com>
2022-04-19 19:47:41 +0100

    Login Page Done Do Registration Page Next

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,16 +1,39 @@
 import "./rightBar.css";
 //import{Users} from "./users";
 import Online from "../online/Online";
-export default function RightBar() {
-  return (
-    <div className="rightBar">
-      <div className="rightBarWrapper">
-        <h4 className="rightBarTitle">Online Friends</h4>
+export default function RightBar(profile) {
+  constHomeRightBar = () => {
+    return(
+      <>
+          <h4 className="rightBarTitle">Online Friends</h4>
         <ul className="rightBarFriendsList">
           {Users.map(u=>(
             <Online key ={u.id} user={u}/>
           ))}
         </ul>
+
+      </>
+    );
+  };
+
+  const ProfileRightBar = () =>{
+    return(
+      <>
+      <h4 className="rightBarTitle"> User Friends</h4>
+      <div className="rightBarProfileFriends">
+        <div className="rightBarProfileFriend">
+          <img src="filler" alt="" className="rightBarProfileFriendImg" />
+          <span className="rightBarProfileFriendName">filler</span>
+        </div>
+      </div>
+     
+      </>
+    )
+  }
+  return (
+    <div className="rightBar">
+      <div className="rightBarWrapper">
+        <ProfileRightBar/>
       </div>
     </div>
   )
